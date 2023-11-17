aadfddc100aef96e4bd2e3723da71920d0a1a945
k0ntinuum <k0ntinuum@proton.me>
2023-11-17 09:28:44 -0500

    ready

--- randomize.c
+++
@@ -1,8 +0,0 @@
-
-void randomize_key(u32 k[N]) {
-    for (u32 i = 0; i < N; i++) 
-            k[i] = arc4random_uniform(B);
-}
-void randomize_text(u32* t) {
-	for (u32 i = 0; i < T; i++) t[i] = arc4random_uniform(B);
-}
