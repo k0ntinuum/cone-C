5400c5fc17b46ffd2b7017c1ffe247152cc867b1
k0ntinuum <k0ntinuum@proton.me>
2023-11-17 19:20:57 -0500

    a

--- source/cone.c
+++ source/cone.c
@@ -3,13 +3,12 @@
 #include <stdbool.h>
 #include <inttypes.h>
 #include <unistd.h>
-typedef u_int32_t u32;
 typedef int64_t i64;
-const u32 N =27;
-const u32 H = 3;//3^3 = 27; center of 27 is 27/2 + 1 = 14
-const u32 center = 0;
-const u32 T = 27;
-const u32 B = 3;
+const i64 N =27;
+const i64 H = 3;//3^3 = 27; center of 27 is 27/2 + 1 = 14
+const i64 center = 0;
+const i64 T = 27;
+const i64 B = 3;
 
 char alph[] = "O|@";
 #include"shuffle.c"
